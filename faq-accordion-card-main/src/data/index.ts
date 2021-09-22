be00bb52e7e90c3590dd85a3e6ccbe2c34914e9a
Krisdina <77259488+krisdinakr@users.noreply.github.com>
2021-09-22 20:36:01 +0700

    add data folder

---
+++ faq-accordion-card-main/src/data/index.ts
@@ -0,0 +1,32 @@
+export const faqs = [
+  {
+    key: 1,
+    title: 'How many team members can I invite?',
+    description:
+      'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.',
+  },
+  {
+    key: 2,
+    title: 'What is the maximum file upload size?',
+    description:
+      'No more than 2GB. All files in your account must fit your allotted storage space.',
+  },
+  {
+    key: 3,
+    title: 'How do I reset my password?',
+    description: `Click “Forgot password” from the login page or “Change password” from your profile page.
+    A reset link will be emailed to you.`,
+  },
+  {
+    key: 4,
+    title: 'Can I cancel my subscription?',
+    description:
+      'Yes! Send us a message and we’ll process your request no questions asked.',
+  },
+  {
+    key: 5,
+    title: 'Do you provide additional support?',
+    description:
+      'Chat and email support is available 24/7. Phone lines are open during normal business hours.',
+  },
+];
