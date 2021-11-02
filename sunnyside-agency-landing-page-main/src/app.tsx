3c650549802c24cdb50e8b268f85817e36c01883
Krisdina <77259488+krisdinakr@users.noreply.github.com>
2021-11-02 11:56:22 +0700

    sunnyside: initial commit

---
+++ sunnyside-agency-landing-page-main/src/app.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from 'components';
+
+const App = () => {
+  return (
+    <div className="landing-page">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App;
---
+++ sunnyside-agency-landing-page-main/src/components/navbar.tsx
@@ -0,0 +1,13 @@
+export const Navbar = () => (
+  <nav className="navbar">
+    <div className="navbar__wrapper">
+      <a href="/" className="navbar__brand">sunnyside</a>
+      <div className="navbar__links">
+        <a href="/" className="navbar__link">About</a>
+        <a href="/" className="navbar__link">Services</a>
+        <a href="/" className="navbar__link">Projects</a>
+        <a href="/" className="navbar__link">Contact</a>
+      </div>
+    </div>
+  </nav>
+);
