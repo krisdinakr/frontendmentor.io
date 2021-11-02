281d94444aa014a6833bad910731450efa165ac3
Krisdina <77259488+krisdinakr@users.noreply.github.com>
2021-11-02 11:19:05 +0700

    faq-accordion

--- faq-accordion-card-main/src/components/Card.tsx
+++ faq-accordion-card-main/src/components/Card.tsx
@@ -1,15 +1,35 @@
 import Accordion from './Accordion';
 import { faqs } from 'data';
-import illustrationImg from 'assets/images/illustration-woman-online-mobile.svg';
+// import desktopImg from 'assets/images/illustration-woman-online-desktop.svg';
+// import mobileImg from 'assets/images/illustration-woman-online-mobile.svg';
+import box from 'assets/images/illustration-box-desktop.svg';
+import { useEffect, useState } from 'react';
 
-export const Card = () => (
-  <div className="card">
-    <div className="card__image">
-      <img src={illustrationImg} alt="" />
+export const Card = () => {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 992);
+
+  const updateMedia = () => setIsDesktop(window.innerWidth > 992);
+
+  useEffect(() => {
+    window.addEventListener('resize', updateMedia);
+    return () => window.removeEventListener('resize', updateMedia);
+  });
+
+  return (
+    <div className="card">
+      <div className="card__image">
+        <div className="card__bg-1"></div>
+        <div className="card__bg-2"></div>
+      </div>
+      {isDesktop && (
+        <div className="card__wrapper">
+          <img src={box} alt="" className="card__box" />
+        </div>
+      )}
+      <div className="card__body">
+        <h1 className="card__header">FAQ</h1>
+        <Accordion data={faqs} />
+      </div>
     </div>
-    <div className="card__body">
-      <h2 className="card__header">FAQ</h2>
-      <Accordion data={faqs} />
-    </div>
-  </div>
-);
+  );
+};
